656c5ffe9e4594a53bd7319cee257e06175af6fb
Techno India <133761719+TechnoIndian@users.noreply.github.com>
2024-06-23 21:20:31 +0530

    Frida-Tools-in-Termux.sh

--- Frida-Tools-in-Termux.sh
+++ Frida-Tools-in-Termux.sh
@@ -1,12 +1,21 @@
 clear                     
-termux-setup-storage -y
-pkg update -y --assume-yes -o Dpkg::Options::="--force-confold"
-pkg upgrade -y
-pkg install proot-distro -y
-proot-distro install ubuntu
-echo "proot-distro login ubuntu" > $HOME/../usr/bin/fsu
-chmod +x $HOME/../usr/bin/fsu
-clear && cd && rm Ubuntu-in-Termux.sh && clear
+termux-setup-storage
+apt update -y && apt upgrade --assume-yes -o Dpkg::Options::="--force-confold"
+apt install -y build-essential python python-pip git wget binutils openssl
+cd $PREFIX/lib/python3.11/__pycache__ && _file=$(find $PREFIX/lib/python3.11/_sysconfigdata*.py) && rm -rf $PREFIX/lib/python3.11/__pycache__ && sed -i 's|-fno-openmp-implicit-rpath||g' "$_file"
+cd $TMPDIR
+pip install -U setuptools wheel
+pip cache purge
+pip download frida==16.2.1
+gzip -d *.gz && tar -xvf *.tar && rm *.tar && cd frida-16*
+latest_version=$(curl -s https://api.github.com/repos/frida/frida/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/') && curl -L -o frida-core-devkit-${latest_version}-android-arm64.tar.xz https://github.com/frida/frida/releases/download/${latest_version}/frida-core-devkit-${latest_version}-android-arm64.tar.xz
+unxz *.xz && tar -xvf *.tar && export FRIDA_CORE_DEVKIT=$PWD
+pip wheel .
+pip install *.whl frida-tools
+pip cache purge
+cd ..
+rm -rf frida-16*
+cd && clear
 termux-open-url https://t.me/rktechnoindians
 
 RED="\e[31m"
@@ -37,10 +46,6 @@ echo -e "${GREEN}︻デ═一 @rktechnoindians${ENDCOLOR}"
 echo -e "${GREEN}︻デ═一 @RK_TECHNO_INDIA${ENDCOLOR}"
 echo -e "${RED}🚩 ࿗ Jai Shree Ram ࿗ 🚩
      🛕🛕🙏🙏🙏🛕🛕${ENDCOLOR}"
-
- 
-
-echo -e "\e[32m===========================================\e[0m" && echo -e "\e[1;34m[*] \e[32mGreat ! Ubuntu Installed Successfully..\e[0m" && echo -e "\e[32m===========================================\e[0m"
-echo -e "${PINK}︻デ═一 Always use 'fsu' to Run Ubuntu ${ENDCOLOR}"
-echo -e "${PINK}︻デ═一 Always use 'exit' to Logout Ubuntu ${ENDCOLOR}"
-fsu
+     
+     
+echo -e "\e[32m===========================================\e[0m" && echo -e "\e[1;34m[*] \e[32mGreat ! \e[1;31mFЯIDA \e[32mInstalled Successfully..\e[0m" && echo -e "\e[32m===========================================\e[0m"
