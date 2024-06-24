d9a5ce8060450d70aa8ad0602b8adc3ec93f2a7d
Techno India <133761719+TechnoIndian@users.noreply.github.com>
2024-06-24 09:41:57 +0530

    Blutter-in-Termux.sh

--- Blutter-in-Termux.sh
+++ Blutter-in-Termux.sh
@@ -1,22 +1,10 @@
 clear                     
-termux-setup-storage
-apt update -y && apt upgrade --assume-yes -o Dpkg::Options::="--force-confold"
-apt install -y build-essential python python-pip git wget binutils openssl
-cd $PREFIX/lib/python3.11/__pycache__ && _file=$(find $PREFIX/lib/python3.11/_sysconfigdata*.py) && rm -rf $PREFIX/lib/python3.11/__pycache__ && sed -i 's|-fno-openmp-implicit-rpath||g' "$_file"
-cd $TMPDIR
-pip install -U setuptools wheel
-pip cache purge
-pip download frida==16.2.1
-gzip -d *.gz && tar -xvf *.tar && rm *.tar && cd frida-16*
-latest_version=$(curl -s https://api.github.com/repos/frida/frida/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/') && curl -L -o frida-core-devkit-${latest_version}-android-arm64.tar.xz https://github.com/frida/frida/releases/download/${latest_version}/frida-core-devkit-${latest_version}-android-arm64.tar.xz
-unxz *.xz && tar -xvf *.tar && export FRIDA_CORE_DEVKIT=$PWD
-pip wheel .
-pip install *.whl frida-tools
-pip cache purge
-cd ..
-rm -rf frida-16*
-cd && rm Frida-Tools-in-Termux.sh && clear
-termux-open-url https://t.me/rktechnoindians
+apt update -y && apt upgrade -y
+pkg install -y git cmake ninja build-essential pkg-config libicu capstone fmt python-pip wget zip ripgrep neovim
+pip install requests pyelftools pycryptodome
+wget https://github.com/dedshit/blutter-termux/archive/refs/heads/main.zip
+unzip main.zip && cd blutter-termux-main && zip -r main.zip . && mv main.zip $HOME && cd $HOME && unzip main.zip && rm -rf blutter-termux-main && rm main.zip
+clear && termux-open-url https://t.me/rktechnoindians/764
 
 RED="\e[31m"
 PINK="\e[95m"
@@ -48,4 +36,4 @@ echo -e "${RED}🚩 ࿗ Jai Shree Ram ࿗ 🚩
      🛕🛕🙏🙏🙏🛕🛕${ENDCOLOR}"
      
      
-echo -e "\e[32m===========================================\e[0m" && echo -e "\e[1;34m[*] \e[32mGreat ! \e[1;31mFЯIDA \e[32mInstalled Successfully..\e[0m" && echo -e "\e[32m===========================================\e[0m"
+echo -e "\e[32m===============================================\e[0m" && echo -e "\e[1;34m[*] \e[32mGreat ! \e[1;31mB(l)utter \e[32mInstalled Successfully..\e[0m" && echo -e "\e[32m===============================================\e[0m"
