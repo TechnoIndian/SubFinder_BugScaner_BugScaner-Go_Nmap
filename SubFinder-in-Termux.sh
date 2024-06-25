a1bff25bf3e57bb0603710f5a197275f49055cc0
Techno India <133761719+TechnoIndian@users.noreply.github.com>
2024-06-25 08:48:06 +0530

    SubFinder-in-Termux.sh

--- SubFinder-in-Termux.sh
+++ SubFinder-in-Termux.sh
@@ -1,10 +1,9 @@
 clear
 termux-setup-storage
 apt update -y && apt upgrade -y
-pkg install -y git cmake ninja build-essential pkg-config libicu capstone fmt python-pip wget zip ripgrep neovim
-pip install requests pyelftools pycryptodome
-wget https://github.com/dedshit/blutter-termux/archive/refs/heads/main.zip
-unzip main.zip && cd blutter-termux-main && zip -r main.zip . && mv main.zip $HOME && cd $HOME && unzip main.zip && rm -rf blutter-termux-main && rm main.zip
+echo 'PATH="$PATH:$HOME/go/bin"' >> $HOME/.bashrc && source $HOME/.bashrc
+pkg install golang -y
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 cd && clear
 termux-open-url https://t.me/rktechnoindians/764
 
@@ -38,4 +37,4 @@ echo -e "${RED}🚩 ࿗ Jai Shree Ram ࿗ 🚩
      🛕🛕🙏🙏🙏🛕🛕${ENDCOLOR}"
      
      
-echo -e "\e[32m===============================================\e[0m" && echo -e "\e[1;34m[*] \e[32mGreat ! \e[1;31mB(l)utter \e[32mInstalled Successfully..\e[0m" && echo -e "\e[32m===============================================\e[0m"
+echo -e "\e[32m===============================================\e[0m" && echo -e "\e[1;34m[*] \e[32mGreat ! \e[1;31m𝐒𝐮𝐛𝐅𝐢𝐧𝐝𝐞𝐫 \e[32mInstalled Successfully..\e[0m" && echo -e "\e[32m===============================================\e[0m"
